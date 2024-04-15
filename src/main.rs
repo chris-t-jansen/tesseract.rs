2adeda73e7dbb6d4e6bd2b83d7fa0aa4976567d3
Chris Jansen <chris+git@claypotfrog.com>
2024-04-15 00:33:01 -0400

    Finished writing, doesn't work

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
 
+use std::{
+    f64::consts::PI,
+    thread::sleep,
+    time::Duration,
+};
+
 const ww: usize = 100;
 const wh: usize = 50;
 
@@ -10,6 +16,10 @@ const from4: [f64; 4] = [5.0, 0.0, 0.0, 0.0];
 const to4: [f64; 4] =  [0.0, 0.0, 0.0, 0.0];
 const up4: [f64; 4] = [0.0, 1.0, 0.0, 0.0];
 const over4: [f64; 4] = [0.0, 0.0, 1.0, 0.0];
+
+const from3: [f64; 3] = [3.00, 0.99, 1.82];
+const to3: [f64; 3] = [0.0, 0.0, 0.0];
+const up3: [f64; 3] = [0.0, -1.0, 0.0];
 
 #[derive(Clone, Copy)]
 struct Coord {
@@ -22,6 +32,36 @@ impl Coord {
         Coord{
             x: 0,
             y: 0,
+        }
+    }
+
+    pub fn build(x_coord: f64, y_coord: f64) -> Self {
+        let x_trunc: f64 = x_coord.trunc();
+        let mut x_short: i16 = 0;
+        if x_trunc > f64::from(i16::MAX) {
+            x_short = i16::MAX;
+        } else if x_trunc < f64::from(i16::MIN) {
+            x_short = i16::MIN;
+        } else {
+            x_short = x_trunc as i16;
+        }
+
+        let y_trunc: f64 = y_coord.trunc();
+        let mut y_short: i16 = 0;
+        if y_trunc > f64::from(i16::MAX) {
+            y_short = i16::MAX;
+        } else if y_trunc < f64::from(i16::MIN) {
+            y_short = i16::MIN;
+        } else {
+            y_short = y_trunc as i16;
+        }
+
+        let x_usize: usize = usize::try_from(x_short).expect("y_short was negative! Conversion to usize failed!");
+        let y_usize: usize = usize::try_from(y_short).expect("y_short was negative! Conversion to usize failed!");
+
+        Coord {
+            x: x_usize,
+            y: y_usize,
         }
     }
 
@@ -41,6 +81,7 @@ fn clr(char_arr: &mut [char; ww * wh]) {
 }
 
 fn set(char_arr: &mut [char; ww * wh], pt: &Coord, character: char) {
+    println!("X: {} | Y: {}", pt.x, pt.y);
     char_arr[pt.y * ww + pt.x] = character;
 }
 
@@ -94,7 +135,7 @@ fn ln(char_arr: &mut [char; ww * wh], a: &mut Coord, b: Coord) {
 
         if e2 < dy {
             err += dx;
-            a.y += usize::try_from(sy).expect("Couldn't convert `sy` to usize!");
+            a.y += usize::try_from(sy).unwrap_or(0);
         }
     }
 
@@ -125,7 +166,7 @@ fn ln(char_arr: &mut [char; ww * wh], a: &mut Coord, b: Coord) {
 
         if e2 < dy {
             err += dx;
-            a.y += usize::try_from(sy).expect("Couldn't convert `sy` to usize!");
+            a.y += usize::try_from(sy).unwrap_or(0);
         }
     }
 
@@ -244,7 +285,7 @@ fn vecScale4(vec: &mut [f64; 4], m: f64) {
 	vec[3] *= m;
 }
 
-fn matVecMul4(result: &mut [f64; 4], mat: [f64; 4], vec: [f64; 4]) {
+fn matVecMul4(result: &mut [f64; 4], mat: [f64; 16], vec: [f64; 4]) {
     for row in 0..4 {
         result[row] = 0.0;
 
@@ -318,6 +359,186 @@ fn view4(result: &mut [f64; 16]) {
     }
 }
 
+fn projectTo3D(vAngle: f64, matView: [f64; 16], matRotation: [f64; 16], verts: &mut Verts) {
+    let mut Wa: [f64; 4] = [0.0; 4];
+    let mut Wb: [f64; 4] = [0.0; 4];
+    let mut Wc: [f64; 4] = [0.0; 4];
+    let mut Wd: [f64; 4] = [0.0; 4];
+
+    for i in 0..4 {
+        Wa[i] = matView[i + 0];
+        Wb[i] = matView[i + 4];
+        Wc[i] = matView[i + 8];
+        Wd[i] = matView[i + 12];
+    }
+
+    let mut s: f64;
+    let t: f64 = 1.0 / (vAngle / 2.0).tan();
+
+    for i in 0..16 {
+        let mut v:[f64; 4] = [0.0; 4];
+        matVecMul4(&mut v, matRotation, verts.v4[i]);
+
+        let mut vf: [f64; 4] = [0.0; 4];
+        vecSub4(&mut vf, v, from4);
+
+        s = t / dot4(vf, Wd);
+
+        verts.v3[i][0] = s * dot4(vf, Wa);
+    }
+}
+
+fn dot3(v: [f64; 3], u: [f64; 3]) -> f64 {
+    (v[0] * u[0]) + (v[1] * u[1]) + (v[2] * u[2])
+}
+
+fn norm3(v: [f64; 3]) -> f64 {
+    dot3(v, v).sqrt()
+}
+
+fn cross3(result: &mut [f64; 3], u: [f64; 3], v: [f64; 3]) {
+    result[0] = (u[1] * v[2]) - (u[2] * v[1]);
+    result[1] = (u[2] * v[0]) - (u[0] * v[2]);
+	result[2] = (u[0] * v[1]) - (u[1] * v[0]);
+}
+
+fn vecSub3(result: &mut [f64; 3], a: [f64; 3], b: [f64; 3]) {
+    result[0] = a[0] - b[0];
+	result[1] = a[1] - b[1];
+	result[2] = a[2] - b[2];
+}
+
+fn vecScale3(vec: &mut [f64; 3], m: f64) {
+    vec[0] *= m;
+	vec[1] *= m;
+	vec[2] *= m;
+}
+
+fn matVecMul3(result: &mut [f64; 3], mat: [f64; 9], vec: [f64; 3]) {
+    for row in 0..3 {
+        result[row] = 0.0;
+        for col in 0..3 {
+            result[row] += mat[col * 3 + row] * vec[row];
+        }
+    }
+}
+
+fn rotXZ3(result: &mut [f64; 9], t: f64) {
+    result[0] = t.cos();
+    result[1] = 0.0;
+    result[2] = -t.sin();
+
+    result[3] = 0.0;
+    result[4] = 1.0;
+    result[5] = 0.0;
+
+    result[6] = t.sin();
+    result[7] = 0.0;
+    result[8] = t.cos();
+}
+
+fn view3(result: &mut [f64; 9]) {
+    let mut Va: [f64; 3] = [0.0; 3];
+    let mut Vb: [f64; 3] = [0.0; 3];
+    let mut Vc: [f64; 3] = [0.0; 3];
+
+    for i in 0..3 {
+        Va[i] = result[i + 0];
+        Vb[i] = result[i + 3];
+        Vc[i] = result[i + 6];
+    }
+
+    let mut norm: f64;
+
+    vecSub3(&mut Vc, to3, from3);
+    norm = norm3(Vc);
+    vecScale3(&mut Vc, 1.0 / norm);
+
+    cross3(&mut Va, Vc, up3);
+    norm = norm3(Va);
+    vecScale3(&mut Va, 1.0 / norm);
+
+    cross3(&mut Vb, Va, Vc);
+
+    for i in 0..3 {
+        result[i + 0] = Va[i];
+        result[i + 3] = Vb[i];
+        result[i + 6] = Vc[i];
+    }
+}
+
+fn projectTo2D(vAngle: f64, matView: [f64; 9], matRotation: [f64; 9], verts: &mut Verts) {
+    let mut Va: [f64; 3] = [0.0; 3];
+    let mut Vb: [f64; 3] = [0.0; 3];
+    let mut Vc: [f64; 3] = [0.0; 3];
+
+    for i in 0..3 {
+        Va[i] = matView[i + 0];
+        Vb[i] = matView[i + 3];
+        Vc[i] = matView[i + 6];
+    }
+
+    let mut s: f64;
+    let t: f64 = 1.0 / (vAngle / 2.0).tan();
+
+    for i in 0..16 {
+        let mut v: [f64; 3] = [0.0; 3];
+        matVecMul3(&mut v, matRotation, verts.v3[i]);
+
+        let mut vf: [f64; 3] = [0.0; 3];
+        vecSub3(&mut vf, v, from3);
+
+        s = t / dot3(vf, Vc);
+
+        verts.v2[i][0] = ((ww / 2) as f64) + ((ww as f64) * s * dot3(vf, Va));
+        verts.v2[i][1] = ((wh / 2) as f64) + ((wh as f64) * s * dot3(vf, Vb));
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut d: [char; wh * ww] = [' '; wh * ww];
+
+    let mut main_verts: Verts = Verts::default();
+
+    let mut viewMat4: [f64; 16] = [0.0; 16];
+    view4(&mut viewMat4);
+    let mut rot4: [f64; 16] = [0.0; 16];
+
+    let mut viewMat3: [f64; 9] = [0.0; 9];
+    view3(&mut viewMat3);
+    let mut rot3: [f64; 9] = [0.0; 9];
+
+    let mut rotation: f64 = 0.0;
+
+    loop {
+        rotation += 0.01;
+
+        rotXW4(&mut rot4, rotation);
+        projectTo3D(PI / 3.0, viewMat4, rot4, &mut main_verts);
+
+        rotXZ3(&mut rot3, rotation * 0.3);
+        projectTo2D(PI / 4.0, viewMat3, rot3, &mut main_verts);
+
+        clr(&mut d);
+
+        for i in 0..32 {
+            let a: i32 = main_verts.indices[i][0];
+            let b: i32 = main_verts.indices[i][1];
+            let mut c1: Coord = Coord::build(main_verts.v2[a as usize][0], main_verts.v2[a as usize][1]);
+            let c2: Coord = Coord::build(main_verts.v2[b as usize][0], main_verts.v2[b as usize][1]);
+            ln(&mut d, &mut c1, c2);
+        }
+
+        print!("\x1b[H");
+
+        for k in 0..(ww * wh) {
+            // The buffers are linear, so linebreaks are inserted when the frame is printed.
+            match k % ww {
+                0 => print!("\n"),
+                1.. => print!("{}", d[k])
+            }
+        }
+
+        sleep(Duration::from_secs(1));
+    }
 }
